5fa03ae92076c77f8eb9258c7d9015a369aa798b
patrick brisbin <pbrisbin@gmail.com>
2014-10-30 11:23:28 -0400

    Make real Query values

--- src/Drync/Drive/Api.hs
+++ src/Drync/Drive/Api.hs
@@ -23,6 +23,23 @@ import qualified Data.Text as T
 import Drync.Token
 import Drync.Drive.Item
 
+data Query
+    = TitleEq Text
+    | ParentEq FileId
+    | Query `And` Query
+    | Query `Or` Query
+
+toParam :: Query -> String
+toParam = urlEncode . T.unpack . toParam'
+
+  where
+    toParam' (TitleEq title) = "title = " <> quote title
+    toParam' (ParentEq fileId) = quote fileId <> " in parents"
+    toParam' (p `And` q) = "(" <> toParam' p <> ") and (" <> toParam' q <> ")"
+    toParam' (p `Or` q) = "(" <> toParam' p <> ") or (" <> toParam' q <> ")"
+
+    quote = ("'" <>) . (<> "'")
+
 baseUrl :: String
 baseUrl = "https://www.googleapis.com/drive/v2"
 
@@ -31,12 +48,12 @@ getFile :: OAuth2Tokens -> FileId -> IO (Maybe Item)
 getFile tokens fileId = fmap decode $ simpleHttp $ baseUrl <>
     "/files/" <> T.unpack fileId <> "?access_token=" <> accessToken tokens
 
--- | Find files matching the given query
-getFiles :: OAuth2Tokens -> Text -> IO [Item]
+-- | Find files matching the given query, limited to 1000 results
+getFiles :: OAuth2Tokens -> Query -> IO [Item]
 getFiles tokens query = do
     mlist <- fmap decode $ simpleHttp $ baseUrl <>
         "/files" <> "?access_token=" <> accessToken tokens <>
-        "&q=" <> urlEncode (T.unpack query) <> "&maxResults=1000"
+        "&q=" <> toParam query <> "&maxResults=1000"
 
     return $ case mlist of
         Just (Items items) -> unTrashed items
@@ -44,17 +61,14 @@ getFiles tokens query = do
 
 -- | Return all child Items of the given folder
 getFilesByParent :: OAuth2Tokens -> FileId -> IO [Item]
-getFilesByParent tokens fileId = getFiles tokens $ quote fileId <> " in parents"
+getFilesByParent tokens = getFiles tokens . ParentEq
 
 -- TODO: handle abiguities since this just searches by name
 getFolder :: OAuth2Tokens -> Text -> IO (Maybe Item)
 getFolder tokens path = do
-    folders <- getFiles tokens $ "title = " <> quote path
+    folders <- getFiles tokens $ TitleEq path
 
     when (length folders > 1) $
         putStrLn "warning: folder name returned multiple results"
 
     return $ listToMaybe folders
-
-quote :: Text -> Text
-quote = ("'" <>) . (<> "'")
