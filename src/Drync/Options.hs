524c542cfb6f0fd3b7caedb349bb2affb432d680
patrick brisbin <pbrisbin@gmail.com>
2014-10-31 14:32:53 -0400

    Implement options parsing

--- src/Drync/Options.hs
+++ src/Drync/Options.hs
@@ -4,17 +4,55 @@ module Drync.Options
     ) where
 
 import Data.Text (Text)
+import Options.Applicative
+import System.FilePath (takeFileName)
+import System.Directory (getCurrentDirectory)
+
+import qualified Data.Text as T
 
 data Options = Options
     { oProfile :: String
+    , oRefresh :: Bool
     , oSyncFrom :: FilePath
     , oSyncTo :: Text
     }
 
--- Sample options for testing
 getOptions :: IO Options
-getOptions = return Options
-    { oProfile = "default"
-    , oSyncFrom = "/home/patrick/Downloads"
-    , oSyncTo = "Downloads"
-    }
+getOptions = do
+    cwd <- getCurrentDirectory
+
+    execParser $ parseOptions cwd `withInfo`
+        "Sync a local directory with one on Google Drive"
+
+  where
+    withInfo :: Parser a -> String -> ParserInfo a
+    withInfo opts desc = info (helper <*> opts) $ progDesc desc
+
+parseOptions :: FilePath -> Parser Options
+parseOptions cwd = Options
+    <$> strOption
+        (  short 'p'
+        <> long "profile"
+        <> metavar "NAME"
+        <> value "default"
+        <> help "Use the named profile"
+        )
+    <*> switch
+        (  short 'r'
+        <> long "refresh-oauth"
+        <> help "Ignore cached OAuth2 credentials"
+        )
+    <*> strOption
+        (  short 'f'
+        <> long "sync-from"
+        <> metavar "DIR"
+        <> value cwd
+        <> help "Sync from the given directory"
+        )
+    <*> fmap T.pack (strOption
+        (  short 't'
+        <> long "sync-to"
+        <> metavar "FOLDER"
+        <> value (takeFileName cwd)
+        <> help "Sync to the given folder"
+        ))
